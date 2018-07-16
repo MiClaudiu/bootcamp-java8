8a02c653fb85784075a40f851fddf38bc3c5a02a
Claudiu Minea <claudiu.minea@cegeka.com>
2018-07-16 16:15:33 +0300

    Project setup

---
+++ test/com/cegeka/bootcamp/test/Test1.java
@@ -0,0 +1,13 @@
+package com.cegeka.bootcamp.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test1 {
+
+    @Test
+    public void test() {
+    }
+
+}
